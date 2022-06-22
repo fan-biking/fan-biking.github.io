2efd22b62599757f0a58228f52387300989285ba
Andrea Bruckmeier <andrea.bruckmeier@uibk.ac.at>
2022-06-22 11:57:38 +0200

    Karte auf eGrundkarte Tirol geändert

--- main.js
+++ main.js
@@ -4,9 +4,55 @@ let lat = 47.223193
 let lng = 11.526103
 let zoom = 11
 
-L.map('map').setView([47.223193, 11.526103], 13);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
 
+let innsbruck = {
+    lat: 47.267222,
+    lng: 11.392778,
+    zoom: 9
+};
+
+// WMTS Hintergrundlayer der eGrundkarte Tirol definieren
+const eGrundkarteTirol = {
+    sommer: L.tileLayer(
+        "http://wmts.kartetirol.at/gdi_summer/{z}/{x}/{y}.png", {
+            attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`
+        }
+    ),
+    winter: L.tileLayer(
+        "http://wmts.kartetirol.at/gdi_winter/{z}/{x}/{y}.png", {
+            attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`
+        }
+    ),
+    ortho: L.tileLayer(
+        "http://wmts.kartetirol.at/gdi_ortho/{z}/{x}/{y}.png", {
+            attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`
+        }
+    ),
+    nomenklatur: L.tileLayer(
+        "http://wmts.kartetirol.at/gdi_nomenklatur/{z}/{x}/{y}.png", {
+            attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`,
+            pane: "overlayPane",
+        }
+    )
+}
+
+// eGrundkarte Tirol Sommer als Startlayer
+let startLayer = eGrundkarteTirol.sommer;
+
+// Overlays Objekt für den GPX Track Layer
+let overlays = {
+    gpx: L.featureGroup()
+};
+
+// Karte initialisieren
+let map = L.map("map", {
+    center: [innsbruck.lat, innsbruck.lng],
+    zoom: innsbruck.zoom,
+    layers: [
+        startLayer
+    ],
+});
+
+// Fullscreen control
+L.control.fullscreen().addTo(map);
