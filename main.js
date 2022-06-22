930f85c8603b764387d686eeb38ceac4d0e889e0
Andrea Bruckmeier <andrea.bruckmeier@uibk.ac.at>
2022-06-22 11:47:07 +0200

    Versuch Karte in main.js einzubauen

---
+++ main.js
@@ -0,0 +1,15 @@
+/* Interaktive Karte Skript*/
+
+let lat = 47.223193
+let lng = 11.526103
+let zoom = 11
+
+L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
